9b26da011d4cd8ff1b68189ac4d6c0cbdec12003
rthadani <rohit.thadani@gmail.com>
2018-11-21 08:29:55 -0800

    CHR complete

--- src/distributed/chr.clj
+++ src/distributed/chr.clj
@@ -12,22 +12,22 @@
 (defn assoc-server
   [chr server-id]
   (let [{:keys [replicas ring]} chr
-        new-keys (mapv #(-> (str server-id %)
-                            md5
-                            biginteger
-                            (cons [server-id])) (range 0 replicas))]
+        new-keys (mapv (fn [key-idx]
+                         [(->> (str server-id key-idx) md5 bigint) server-id]) 
+                       (range 0 replicas))]
     (->> (into [] ring)
-         (concat new-keys)
-         (into (sorted-map))
-         (Consistent-Hash-Ring. replicas))))
+       (concat new-keys)
+       (into (sorted-map))
+       (Consistent-Hash-Ring. replicas))))
 
 (defn get-server
   [chr key]
-  (as-> (:ring chr) $
-    (keys $)
-    (binary-search $ 0 (dec count $) key)
-    (get (:ring chr) $)))
-
+  (let [ring-key (-> (str key) md5 bigint)]
+    (->> (:ring chr)
+         (keys)
+         (into [])
+         (binary-search ring-key)
+         (get (:ring chr)))))
 
 (defn dissoc-server
   [chr server-id]
@@ -39,20 +39,40 @@
          (apply dissoc)
          (Consistent-Hash-Ring. replicas))))
 
+;;for hinted handoffs
 (defn neighbors
-  [chr server-id])
+  [chr replica-id]
+  (let [{}]))
 
 (defn- binary-search
-  [ring begin end key]
-  (let [mid (/ (+ begin end) 2)]
-    (cond
-      (= key (ring mid)) (ring mid)
-      (and (zero? mid) (< key (ring 0))) (last ring)
-      (and (= (mid (dec (count ring)))) (> key (ring mid)) (last ring))
-      (and (> key (ring mid)) (< key (ring (inc mid)))) (ring mid)
-      (< (ring mid) key) (binary-search ring begin mid)
-      :else (binary-search ring (inc mid) end))))
+  ([key ring]
+   (let [end (dec (count ring))
+       result (and (pos? end) (binary-search ring 0 end key))]
+     (cond
+       (neg? end) nil
+       (not= result -1) result
+       (< key (ring 0)) (last ring)
+       (> key (ring end))(first ring))))
+  ([ring begin end key]
+   (let [mid (int (/ (+ begin end) 2))]
+     (cond
+       (>= begin end) -1
+       (= (ring mid) key) (ring mid)
+       (and (< 0 mid (count ring)) (> key (ring (dec mid))) (< key (ring (inc mid)))) (ring mid)
+       (< key (ring mid)) (binary-search ring begin mid key)
+       :else (binary-search ring (inc mid) end key)))))
 
-([1 2 3] 0)
-(biginteger (md5 "rohit"))
+#_(binary-search 0 [1 2 4 5])
+#_(binary-search (bigint 0) (mapv #(bigint %) (range 1 6)))
+#_(binary-search 3 [1 2 3 4 5])
+#_(binary-search 6 [1 2 4 5])
+#_(binary-search 3 [1 2 4 5])
+#_(binary-search 1 [1 2 4 5])
+#_(binary-search 1 [])
+#_(def chr (-> (new-chr 3)
+              (assoc-server "192.168.0.1")
+              (assoc-server "192.168.0.2")))
 
+#_ (get-server chr "file1")          
+
+#_(dissoc-server chr "192.168.0.1")
