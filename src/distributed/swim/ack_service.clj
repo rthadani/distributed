34a26d64cce3fd7a993ff8894a17a8dd1e3cdc49
rthadani <rohit.thadani@gmail.com>
2026-09-23 20:03:55 -0700

    Remove broken TCP-based SWIM stubs
    The initial swim namespace stubs referenced aleph/gloss/manifold, which are not project dependencies, and peer.clj did not even parse. Replace them with a clean gRPC-based SWIM implementation, starting from an empty swim directory.

--- src/distributed/swim/ack_service.clj
+++
@@ -1,7 +0,0 @@
-(ns distributed.swim.ack-service
-  (:require []))
-
-(defrecord SwimAckService [membership]
-  AckService
-  (send-ping [_ message ack-received-handler ack-timeout-handler]
-             ))
--- src/distributed/swim/network_peer.clj
+++
@@ -1,1 +0,0 @@
-(ns distributed.swim.network-peer)
